f69a824d06d883e150433c5320f454b44f5c934e
Christian Bundy <christianbundy@fraction.io>
2019-01-15 13:23:46 -0800

    Switch back to vanilla flumedb for rebuild testing

--- test/index.js
+++ test/index.js
@@ -11,38 +11,16 @@ test('append + delete + get + view.get', function (t) {
     t.error(err, 'append success')
     t.equal(seq, 2, 'items added')
     // Delete one.
-    db.del([seq, seq - 1], (err) => {
-      t.error(err, 'delete success')
-      // Ensure the value was deleted from the log.
+    db.rebuild((err) => {
+      t.error(err, 'rebuild success')
       db.get(seq, (err, item) => {
         t.error(err, 'get success')
-        t.equal(item, undefined, 'deleted from log')
+        t.equal(item, undefined, 'still deleted from log')
         // Ensure the value was deleted from the view.
-        db.get(seq - 1, (err, item) => {
-          t.error(err, 'get success')
-          t.equal(item, undefined, 'also deleted from log')
-          // Ensure the value was deleted from the view.
-          db.bool.get(seq, (err, item) => {
-            t.error(err, 'view.get success')
-            t.equal(item, undefined, 'deleted from view')
-            db.bool.get(seq - 1, (err, item) => {
-              t.error(err, 'view.get success')
-              t.equal(item, undefined, 'also deleted from view')
-              db.rebuild((err) => {
-                t.error(err, 'rebuild success')
-                db.get(seq, (err, item) => {
-                  t.error(err, 'get success')
-                  t.equal(item, undefined, 'still deleted from log')
-                  // Ensure the value was deleted from the view.
-                  db.bool.get(seq, (err, item) => {
-                    t.error(err, 'view.get success')
-                    t.equal(item, undefined, 'still deleted from view')
-                    t.end()
-                  })
-                })
-              })
-            })
-          })
+        db.bool.get(seq, (err, item) => {
+          t.error(err, 'view.get success')
+          t.equal(item, undefined, 'still deleted from view')
+          t.end()
         })
       })
     })
